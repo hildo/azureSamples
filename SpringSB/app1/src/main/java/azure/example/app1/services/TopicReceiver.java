432ea317b10cfb63f0d01018544ea12f12cc6f02
Ed <edhillmann@yahoo.com>
2015-10-24 14:57:15 +1000

    Resend ping on receipt of ping

--- SpringSB/app1/src/main/java/azure/example/app1/services/TopicReceiver.java
+++ SpringSB/app1/src/main/java/azure/example/app1/services/TopicReceiver.java
@@ -2,6 +2,7 @@ package azure.example.app1.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +11,18 @@ public class TopicReceiver {
 
     private final Logger logger = LoggerFactory.getLogger(TopicReceiver.class);
     
+    @Autowired
+    private QueuePublisher queuePublisher;
+    
     @JmsListener(destination = "mytopic", subscription = "TopicReceiver")
     public void onMessage(String message) {
         logger.info("Received message from topic: {}", message);
+        try {
+            Thread.sleep(3000L);
+            queuePublisher.sendPing();
+        } catch (InterruptedException ex) {
+        }
+        
     }
     
 }
